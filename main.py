b337f041f2d95edc565a6e04c1e0274c11d722a1
pintuprajapati <pintu1481999@gmail.com>
2023-06-30 14:42:19 +0530

    feat: Request Body and PUT endpoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Instance of FastAPI()
 app = FastAPI()
@@ -62,3 +63,28 @@ async def get_item(item_id: str, q: Optional[str] = None, short: bool = False):
             "description": "The brown fox jumps over the lazy dog"
         })
     return item
+
+##### Request Body #####
+# pydantic model
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+# POST endpoint
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.dict() # it will be converted to dic()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# PUT endpoint
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: Optional[str] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
